bf047efe763324ae46ee301c1baa3befb97997c4
AbiePerdanaKusuma1054 <icikiwerblaemblaem@gmail.com>
2021-05-28 21:38:47 +0700

    list category

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -2,7 +2,45 @@ import 'package:waller/model/categories_model.dart';
 
 String apiKey = "563492ad6f917000010000017a69bec3ee4d4a4f9f6c65477f45d928";
 
-List<CategoriesModel> getCaegories() {
+List<CategoriesModel> getCategories() {
   List<CategoriesModel> categories = new List();
   CategoriesModel categoriesModel = new CategoriesModel();
+//
+  categoriesModel.imgUrl =
+      "https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200";
+  categoriesModel.categoriesName = "Nature";
+  categories.add(categoriesModel);
+  categoriesModel = new CategoriesModel();
+//
+  categoriesModel.imgUrl =
+      "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200";
+  categoriesModel.categoriesName = "Animal";
+  categories.add(categoriesModel);
+  categoriesModel = new CategoriesModel();
+//
+  categoriesModel.imgUrl =
+      "https://images.pexels.com/photos/2129796/pexels-photo-2129796.png?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200";
+  categoriesModel.categoriesName = "City";
+  categories.add(categoriesModel);
+  categoriesModel = new CategoriesModel();
+//
+  categoriesModel.imgUrl =
+      "https://images.pexels.com/photos/3156482/pexels-photo-3156482.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200";
+  categoriesModel.categoriesName = "Cars";
+  categories.add(categoriesModel);
+  categoriesModel = new CategoriesModel();
+//
+  categoriesModel.imgUrl =
+      "https://images.pexels.com/photos/110854/pexels-photo-110854.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200";
+  categoriesModel.categoriesName = "Space";
+  categories.add(categoriesModel);
+  categoriesModel = new CategoriesModel();
+//
+  categoriesModel.imgUrl =
+      "https://images.pexels.com/photos/2693208/pexels-photo-2693208.png?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200";
+  categoriesModel.categoriesName = "Abstract";
+  categories.add(categoriesModel);
+  categoriesModel = new CategoriesModel();
+
+  return categories;
 }
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:waller/data/data.dart';
+import 'package:waller/model/categories_model.dart';
 import 'package:waller/model/wallpaper_model.dart';
 import 'package:waller/page/allcategories.dart';
 import 'package:waller/page/search.dart';
@@ -19,6 +20,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<WallpaperModel> wallpapers = new List();
+  List<CategoriesModel> categories = new List();
 
   getTrendingWallpapers() async {
     var response = await http.get(
@@ -37,6 +39,7 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
+    categories = getCategories();
     getTrendingWallpapers();
     super.initState();
   }
@@ -104,29 +107,43 @@ class _HomeState extends State<Home> {
                 height: 16,
               ),
               Container(
-                  height: 80,
-                  child: ListView(
-                    padding: EdgeInsets.symmetric(horizontal: 24),
+                height: 80,
+                child: ListView.builder(
+                    padding: EdgeInsets.symmetric(horizontal: 18),
+                    itemCount: categories.length,
+                    shrinkWrap: true,
                     scrollDirection: Axis.horizontal,
-                    children: [
-                      CategoriesTile(
-                        imgurl: "assets/images/andre-benz.jpg",
-                        title: "Category",
-                      ),
-                      CategoriesTile(
-                        imgurl: "assets/images/harley-davidson.jpg",
-                        title: "Category",
-                      ),
-                      CategoriesTile(
-                        imgurl: "assets/images/nasa.jpg",
-                        title: "Category",
-                      ),
-                      CategoriesTile(
-                        imgurl: "assets/images/orkhan.jpg",
-                        title: "Category",
-                      ),
-                    ],
-                  )),
+                    itemBuilder: (context, index) {
+                      return CategoriesTile(
+                        title: categories[index].categoriesName,
+                        imgUrl: categories[index].imgUrl,
+                      );
+                    }),
+              ),
+              // Container(
+              //     height: 80,
+              //     child: ListView(
+              //       padding: EdgeInsets.symmetric(horizontal: 24),
+              //       scrollDirection: Axis.horizontal,
+              //       children: [
+              //         CategoriesTile(
+              //           imgurl: "assets/images/andre-benz.jpg",
+              //           title: "Category",
+              //         ),
+              //         CategoriesTile(
+              //           imgurl: "assets/images/harley-davidson.jpg",
+              //           title: "Category",
+              //         ),
+              //         CategoriesTile(
+              //           imgurl: "assets/images/nasa.jpg",
+              //           title: "Category",
+              //         ),
+              //         CategoriesTile(
+              //           imgurl: "assets/images/orkhan.jpg",
+              //           title: "Category",
+              //         ),
+              //       ],
+              //     )),
               wallpaperList(wallpapers: wallpapers, context: context),
               SizedBox(
                 height: 16,
@@ -140,49 +157,39 @@ class _HomeState extends State<Home> {
 }
 
 class CategoriesTile extends StatelessWidget {
-  final String imgurl, title;
-  CategoriesTile({this.imgurl, this.title});
+  final String imgUrl, title;
+  CategoriesTile({@required this.title, @required this.imgUrl});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => Categories()));
-      },
-      child: Container(
+    return Container(
+        child: Stack(children: <Widget>[
+      ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Image.network(
+            imgUrl,
+            height: 50,
+            width: 100,
+            fit: BoxFit.cover,
+          )),
+      Container(
         margin: EdgeInsets.only(right: 4),
-        child: Stack(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.asset(
-                imgurl,
-                height: 50,
-                width: 100,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.black26,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              height: 50,
-              width: 100,
-              alignment: Alignment.center,
-              child: Text(
-                title,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    fontFamily: "Poppins"),
-              ),
-            ),
-          ],
+        decoration: BoxDecoration(
+          color: Colors.black26,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        height: 50,
+        width: 100,
+        alignment: Alignment.center,
+        child: Text(
+          title,
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+              fontFamily: "Poppins"),
         ),
       ),
-    );
+    ]));
   }
 }
